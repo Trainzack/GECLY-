a5f91257b041bcf4696cb7d459c175e131554b7e
GavinMK <gavinkgrn@gmail.com>
2017-01-31 15:22:49 -0800

    Delete Test.java

--- Test.java
+++ Test.java
@@ -4,9 +4,6 @@
 public class Test {
     public static void main(String[] args){
         System.out.println("HELLO IM TESTING IF THIS WORKED");
-        System.out.println("it does not");
-        
-        System.out.println("clara likes potatos");
 
     }
 }
