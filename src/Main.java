fe1e1f43e0db314e7f384d8346eccc1f27686200
eli <eazupke@cpp.edu>
2017-02-02 13:35:12 -0800

    Java Project Base?

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
