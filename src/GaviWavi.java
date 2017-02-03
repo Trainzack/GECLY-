bc1a70f7b572a944489ad4578936b7ace402f3aa
GaviWavi <greeenpulse@gmail.com>
2017-02-02 22:46:52 -0800

    Merge branch 'Gavin's-Version' of https://github.com/Trainzack/GECLY-
    # Conflicts:
    #	README.md

--- src/GaviWavi.java
+++ src/GaviWavi.java
@@ -1,4 +1,3 @@
-package PACKAGE_NAME;
 
 /**
  * @author Gavin Kremer
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,9 @@
 public class Main {
 
 	public Main() {
-		// TODO Auto-generated constructor stub
 	}
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Test!");
 	}
 
