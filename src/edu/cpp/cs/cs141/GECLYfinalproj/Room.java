932a767f6dc04cf0c63483d9f8afb1766924862b
lucasfrias <lafrias2@gmail.com>
2017-02-07 15:55:48 -0800

    Skeletal for the room class.

--- src/edu/cpp/cs/cs141/GECLYfinalproj/Room.java
+++ src/edu/cpp/cs/cs141/GECLYfinalproj/Room.java
@@ -17,88 +17,21 @@ package edu.cpp.cs.cs141.GECLYfinalproj;
  *
  */
 
-import java.io.Serializable;
-
 /**
- * This class represents a room which can contain the briefcase or an item in the game.
- *
- * @author Gavin Kremer
+ * This class Room represents a room in the grid that can contain world items including a briefcase.
  */
-public class Room implements Locatable,Serializable{
-
+public class Room implements Locatable{
     /**
-     * This field represents the contents of the {@link Room}, which can be the {@link Briefcase} or
-     * another {@link WorldItem}
+     * This field will determine if there is a world item inside the room.
      */
-    private WorldItem contents;
-
-    /**
-     * This field represents the ASCII character identifying the object.
-     */
-    private char ASCIIRep;
-
-    /**
-     * This field represents the Unicode character identifying the object.
-     */
-    private char UnicodeRep;
-
-    /**
-     * This is the constructor for {@link Room}
-     */
-    Room(){
-        this.ASCIIRep = 'R';
-        this.UnicodeRep = '◫';
-    }
-
-    /**
-     * Getter for {@link #ASCIIRep}
-     * @return value of {@link #ASCIIRep}
-     */
-    public char getASCIIRep(){
-        return this.ASCIIRep;
-    }
-
-    /**
-     * Getter for {@link #UnicodeRep}
-     * @return value of {@link #UnicodeRep}
-     */
-    public char getUnicodeRep(){
-        return this.UnicodeRep;
-    }
-
-    @Override
-    public Location getLocation() {
-        return null;
-    }
-
+    private boolean ContainedWorldItem;
     @Override
     public char getASCIIDisplayCharacter(boolean visible) {
-        if(visible){
-            return this.getASCIIRep();
-        }
-        return '*';
+        return 0;
     }
 
     @Override
     public char getUnicodeDisplayCharacter(boolean visible) {
-        if (visible) {
-            return this.getUnicodeRep();
-        }
-        return '■';
-    }
-
-    /**
-     * Setter for {@link #contents}
-     * @param item value for {@link #contents}
-     */
-    public void setContents(WorldItem item){
-        this.contents = item;
-    }
-    /**
-     * Getter for {@link #contents}
-     * @return value of {@link #contents}
-     */
-    public WorldItem getContents() {
-        return contents;
+        return 0;
     }
 }
