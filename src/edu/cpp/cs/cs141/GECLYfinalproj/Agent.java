3cc1bbb80e2b79168ac72322b9eabf931d6d3659
GavinMK <gavinkgrn@gmail.com>
2017-02-07 13:00:34 -0800

    Added skeletal methods and attributes to the abstract Agent class.

--- src/edu/cpp/cs/cs141/GECLYfinalproj/Agent.java
+++ src/edu/cpp/cs/cs141/GECLYfinalproj/Agent.java
@@ -16,5 +16,54 @@ package edu.cpp.cs.cs141.GECLYfinalproj;
  * Yan Huang (Lilli)
  *
  */
-public abstract class Agent {
+
+/**
+ * This class represents an agent in the game (enemies and the player) and is the parent class for
+ * {@link Player} and {@link Ninja}.
+ *
+ * @author Gavin Kremer
+ */
+
+public abstract class Agent implements Locatable{
+
+    /**
+     * This field represents the ASCII character which represents the {@link Agent}.
+     */
+    private char ASCIIRep;
+
+    /**
+     * This field represents the Unicode character which represents the {@link Agent}.
+     */
+    private char UnicodeRep;
+
+    /**
+     * This field represents the locale of the {@link Agent}, AKA the {@link Grid} they are located in.
+     */
+    private Grid locale;
+
+    /**
+     * This method will handle the {@link Agent}'s movement, and return wether or not the move was successful
+     * (for if they try to move out of bounds for example.)
+     * @return if the move was successful or not.
+     */
+    public boolean move(){
+        return true;
+    }
+
+    /**
+     * This method will handle the {@link Agent}'s death, which will be radically different depending on which
+     * agent is using it, so it is abstract.
+     */
+    public abstract void killThis();
+
+
+    @Override
+    public char getASCIIDisplayCharacter(boolean visible) {
+        return 0;
+    }
+
+    @Override
+    public char getUnicodeDisplayCharacter(boolean visible) {
+        return 0;
+    }
 }
