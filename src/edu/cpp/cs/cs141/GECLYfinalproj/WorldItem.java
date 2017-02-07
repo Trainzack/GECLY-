bf13b82ab225be00b8660e0e2cc8b5aa27c10dfa
GavinMK <gavinkgrn@gmail.com>
2017-02-07 14:44:02 -0800

    Fixed some sections and added getters.

--- src/edu/cpp/cs/cs141/GECLYfinalproj/WorldItem.java
+++ src/edu/cpp/cs/cs141/GECLYfinalproj/WorldItem.java
@@ -22,7 +22,7 @@ package edu.cpp.cs.cs141.GECLYfinalproj;
  * abstract class holds all the necessary attributes for the actual items' classes extending this class, like 
  * name, a char that represents the item, and the grid for the items' locations. 
  *
- * @author Clara Nguyen
+ * @author Clara Nguyen, Gavin Kremer
  */
 public abstract class WorldItem implements Locatable {
 	
@@ -30,34 +30,78 @@ public abstract class WorldItem implements Locatable {
 	 * The String name field represents the name of the world item.
 	 */
 	private String name;
-	
+
+    /**
+     * This field represents the ASCII character which represents the {@link WorldItem}.
+     */
+    private char ASCIIRep;
+
+    /**
+     * This field represents the Unicode character which represents the {@link WorldItem}.
+     */
+    private char UnicodeRep;
+
 	/**
-	 * This Grid object field represents the grid that the object's are on .
+	 * This Grid object field represents the grid that the object's are on.
 	 */
 	private Grid locale;
-	
-	/* (non-Javadoc)
-	 * @see edu.cpp.cs.cs141.GECLYfinalproj.Locatable#getASCIIDisplayCharacter(boolean)
+
+    /**
+     * This method applies the item to the character. It is abstract so that it can be applied uniquely
+     * to the different classes that extend this abstract class.
+     */
+    public abstract void apply();
+
+    /**
+     * This is the constructor for {@link WorldItem}
+     */
+    WorldItem(){
+
+    }
+
+	/**
+	 * Getter for {@link #name}
+	 * @return value of {@link #name}
 	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Getter for {@link #ASCIIRep}
+	 * @return value of {@link #ASCIIRep}
+	 */
+	public char getASCIIRep() {
+		return ASCIIRep;
+	}
+
+	/**
+	 * Getter for {@link #UnicodeRep}
+	 * @return value of {@link #UnicodeRep}
+	 */
+	public char getUnicodeRep() {
+		return UnicodeRep;
+	}
+
+	/**
+	 * Getter for {@link #locale}
+	 * @return value of {@link #locale}
+	 */
+	public Grid getLocale() {
+		return locale;
+	}
+
+
+
 	@Override
 	public char getASCIIDisplayCharacter(boolean visible) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.cpp.cs.cs141.GECLYfinalproj.Locatable#getUnicodeDisplayCharacter(boolean)
-	 */
 	@Override
 	public char getUnicodeDisplayCharacter(boolean visible) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
-	
-	/**
-	 * This method applies the item to the character. It is abstract so that it can be applied uniquely
-	 * to the different classes that extend this abstract class.
-	 */
-	public abstract void apply();
+
 	
 }
