c3bd960c323a87d37279a21ebd8d0a4d5e5f266a
eli <eazupke@cpp.edu>
2017-02-08 12:43:07 -0800

    Removed 'static' from UserInterface methods to actually allow for polymorphism.

--- src/edu/cpp/cs/cs141/GECLYfinalproj/UserInterface.java
+++ src/edu/cpp/cs/cs141/GECLYfinalproj/UserInterface.java
@@ -29,28 +29,28 @@ public abstract class UserInterface {
      * This method essentially updates the boardstate, which shows the new locations of objects and their
      * visibility.
      */
-    public static void updateBoardState(){
+    public void updateBoardState(){
 
     }
 
     /**
      * This method starts the game and is called from the {@link Main} method.
      */
-    public static void startGame(){
+    public void startGame(){
 
     }
 
     /**
      * This method shows the options menu where players can change the setting of the game.
      */
-    public static void openOptions(){
+    public void openOptions(){
 
     }
 
     /**
      * This method handles the user quitting the game, and essentially just terminates the program.
      */
-    public static void quitGame(){
+    public void quitGame(){
 
     }
 }
