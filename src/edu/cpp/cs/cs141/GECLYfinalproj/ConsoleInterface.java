95e475d7ef43755651c8e47388f4d555480a28f3
eli <eazupke@cpp.edu>
2017-02-08 12:19:23 -0800

    Adds menu methods, for use in constructing menus (based off of my submission to homework 2).

--- src/edu/cpp/cs/cs141/GECLYfinalproj/ConsoleInterface.java
+++ src/edu/cpp/cs/cs141/GECLYfinalproj/ConsoleInterface.java
@@ -1,6 +1,7 @@
 package edu.cpp.cs.cs141.GECLYfinalproj;
 
 import java.io.PrintWriter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -23,31 +24,74 @@ import java.util.Scanner;
 /**
  * This class represents the console based interface which will involve console elements like the Scanner and System.out.
  *
- * @author Gavin Kremer
+ * @author Gavin Kremer, Eli Zupke
  */
 public class ConsoleInterface extends UserInterface{
 
     /**
      * This field represents the Scanner that the user will use to interact with the UI.
      */
-    private Scanner SWIFT = new Scanner(System.in);
+    private Scanner userInput = new Scanner(System.in);
     
     /**
      * The PrintWriter that will be used for output (works the same as System.out). Note that we will need to make considerations for Unicode support by using an OutputStreamWriter with the right encoding as a first argument. If that doesn't work at runtime, we can default to ASCII by setting {@link #UnicodeEnabled}.
      */
-    private PrintWriter SystemOutput;
+    private PrintWriter systemOutput;
     
     /**
      * Whether or not output can contain Unicode.
      */
-    private boolean UnicodeEnabled;
+    private boolean unicodeEnabled;
 
     /**
      * This method will print a method in the console.
      */
-    public static void showMessage(){
-
+    public void showMessage(String message){
+    	systemOutput.println(message);
     }
+    
+    /**
+	 * Creates a menu based off of an array of strings, and returns the array index of the string stat was selected.
+	 * 
+	 * @param choices the options that will be given to the player
+	 * @param query the text to prompt the player with
+	 * @return the array index of the choice that was selected
+	 */
+	public int displayMenu(String[] choices) {
+		
+		for (int i = 0; i < choices.length; i++) {
+			showMessage((i+1) + ". " + choices[i]);
+		}
+		
+		return getInt(1,choices.length)-1;
+	}
+	
+	/**
+	 * Waits for the user to enter a valid number that is inside the specified range, then returns the result. Used for letting the player select menu choices.
+	 * 
+	 * @param min the smallest number that will be accepted
+	 * @param max the largest number that will be accepted
+	 * @return the int that the player selected
+	 */
+	private int getInt(int min, int max) {
+		
+		while (true) { //This loop will ensure the only way out of this method is for a valid option to be selected.
+			try {
+				int output = userInput.nextInt();
+				if (output < min || output > max) {
+					//This catches cases where the user inputs a number, but it is not within the allowed range.
+					showMessage("Invalid number. Please enter a number from " + min + " to " + max + ".\n");
+				} else {
+					return output;
+				}
+			} catch (InputMismatchException e)  {
+				//This catches cases where the user did not actually enter a number at all.
+				showMessage("Invalid number. Please enter a valid number.\n");
+				userInput.next(); //This line is required for the method to function, and I don't know why.
+			}
+		}
+		
+	}
 
 
 
