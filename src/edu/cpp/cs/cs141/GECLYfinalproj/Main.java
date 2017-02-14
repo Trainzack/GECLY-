d4e67dd724db6e02e1560a6d77017a79660d2e31
eli <eazupke@cpp.edu>
2017-02-14 13:19:16 -0800

    Added debug mode switch in main, cleaned up consoleInterface.

--- src/edu/cpp/cs/cs141/GECLYfinalproj/Main.java
+++ src/edu/cpp/cs/cs141/GECLYfinalproj/Main.java
@@ -34,10 +34,11 @@ public class Main {
 
 	//I left this method in for now so you guys can test it if you want. Will delete for final release. change line 39 to false to see without debug mode.
 	public static void GavinsPrinttest(){
-		ConsoleInterface GavinsInt = new ConsoleInterface();
-		GavinsInt.startGame();
-		GavinsInt.setDebugging(true);
-		GavinsInt.displayGrid();
+		ConsoleInterface gavinsInt = new ConsoleInterface();
+		gavinsInt.startGame();
+		gavinsInt.showMessage("Debug mode enabled?");
+		gavinsInt.setDebugging(gavinsInt.displayMenu());
+		gavinsInt.displayGrid();
 	}
 }
 
